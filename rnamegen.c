0bab55fabc2fbb0bee4ff2571fa6ed68ee2106be
starmlerp <mlerp.blop@gmail.com>
2022-05-15 02:41:18 +0200

    added code to better select given letter based on available data and fixed bug where null terminator data was not recorded properly

--- rnamegen.c
+++ rnamegen.c
@@ -2,12 +2,13 @@
 #include <stdlib.h> 
 #include <time.h>
 
-#define MINLEN 4
-#define MAXLEN 11
 
 #define SEPARATOR ','
 
 #define random(x, y) (rand() % (y - x + 1) + x)
+
+#define FSELECTOR letters[i].weight * letters[i].preceede['\0']
+#define LSELECTOR letters[t].succeede[i] * letters[i].weight * letters[i].preceede[t]
 
 struct letterdata{
 	int weight, *preceede;
@@ -89,15 +90,21 @@ struct letterdata *analyze(char **sample, size_t size){
 		for(size_t j = 0; j < charwidth; j++)out[i].preceede[j] = 0;
 	}
 	for(size_t i = 0; i < size; i++){
-		int c = 0;
-		while(sample[i][c] != '\0'){
-			out[sample[i][c]].weight++;
-			out[sample[i][c]].succeede[sample[i][c+1]]++;
-			if(!c)out[sample[i][c]].preceede['\0']++;
-			else out[sample[i][c]].preceede[sample[i][c-1]]++;
+		int c = -1;
+		do{
 			c++;
-		}
+			char letter = sample[i][c];
+			out[sample[i][c]].weight++;
+			if(sample[i][c])out[sample[i][c]].succeede[sample[i][c+1]]++;
+			if(!c){
+				out[sample[i][c]].preceede['\0']++;
+//				out['\0'].weight++;
+//				out['\0'].succeede[sample[i][c]]++;
+			}
+			else out[sample[i][c]].preceede[sample[i][c-1]]++;
+		}while(sample[i][c] != '\0');
 	}
+	out['\0'].weight/=4;
 	return out;
 }
 
@@ -108,29 +115,31 @@ char *assemble(struct letterdata *letters){
 	size_t charwidth = 1, s = 0;
 	for(size_t i = 0; i < 8 * sizeof(char); i++)charwidth *= 2;
 
-	for(size_t i = 0; i < charwidth; i++)sum+=letters[i].weight;
+	for(size_t i = 0; i < charwidth; i++)sum+=(FSELECTOR);
 	struct word *f, *c = (struct word*)malloc(sizeof(struct word));
 	c->next = NULL;
 	f = c;
 	long r = random(1, sum);
 	for(size_t i = 0; i < charwidth && r > 0; i++){
-		r -= letters[i].weight;
+		r -= (FSELECTOR);
 		c->letter = i;
 	}
 	s++;
 	while(c->letter){
 		int t = c->letter;
-//		printf("%d\n", c->letter);
 		c->next = (struct word*)malloc(sizeof(struct word));
 		c = c->next;
 		c->next = NULL;
 		long lsum = 0;
-		for(size_t i = 0; i < charwidth; i++)lsum+=letters[t].succeede[i];
+		for(size_t i = 0; i < charwidth; i++)lsum+=(LSELECTOR);
 		long r = random(1, lsum);
+//		printf("%d/%d\n", r, lsum);
 		for(size_t i = 0; i < charwidth && r > 0; i++){
 			c->letter = i;
-			r -= letters[t].succeede[i];
+			r -= (LSELECTOR);
+//			printf("%d\n", r);
 		}
+//		printf("%c", c->letter);
 		s++;
 	}
 	char *out = (char*)malloc(s * sizeof(char));
@@ -153,9 +162,9 @@ int main(int argc, char *argv[]){
 	struct letterdata *data = analyze(names, n);
 /*
 	for(int i = 0; i < 256; i++){
-		if(data[i].weight)printf("%c: %d\n", i, data[i].weight);
-		for(int j = 0; j < 256; j++)if(data[i].succeede[j])printf("\t%c: %d:%d\n", j,data[i].preceede[j], data[i].succeede[j]);
-	}
+		if(data[i].weight)printf("%c(%d): %d\n", i, i, data[i].weight);
+		for(int j = 0; j < 256; j++)if(data[i].succeede[j] || data[i].preceede[j])printf("\t%c(%d): %d:%d\n", j, j, data[i].preceede[j], data[i].succeede[j]); }
 */
+//	printf("%d\n", n);
 	for(int i = 0; i < count; i++)printf("%s\n", assemble(data));
 }
