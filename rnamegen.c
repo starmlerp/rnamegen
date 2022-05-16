f4035123402ff8d949740dc2b0fb7faa3a577367
starmlerp <mlerp.blop@gmail.com>
2022-05-16 20:32:38 +0200

    better organized dataset, added structure to track word length data, fixed input reading, fixed a bug where a lot of the output names are single characters

--- rnamegen.c
+++ rnamegen.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <time.h>
-
+#include <math.h>
 
 #define SEPARATOR ','
 
 #define random(x, y) (rand() % (y - x + 1) + x)
 
-#define FSELECTOR letters[i].weight * letters[i].preceede['\0']
-#define LSELECTOR letters[t].succeede[i] * letters[i].weight * letters[i].preceede[t]
+#define FSELECTOR letters.ldata[i].weight * letters.ldata[i].preceede['\0']
+#define LSELECTOR letters.ldata[t].succeede[i] * letters.ldata[i].weight * letters.ldata[i].preceede[t] * (i?1:nsize/namelen)
 
 struct letterdata{
 	int weight, *preceede;
 	int *succeede;
+};
+
+struct stats{
+	struct letterdata *ldata;
+	size_t ltotal;
+	size_t cnames;
 };
 
 struct word{
@@ -34,11 +40,12 @@ int load(FILE* f, char ***out){
 		if(inp == SEPARATOR){
 			n++;
 			inp = fgetc(f);
-			while(inp != ' ' && inp != EOF)inp = fgetc(f);
+			while(inp == ' ' || inp == '\n')inp = fgetc(f);
 		}
 	}while(inp != EOF);
 	
 	rewind(f);
+	inp = fgetc(f);
 	char **names = (char**)malloc(n * sizeof(char*));
 	*out = names;
 	int m[n];
@@ -46,73 +53,80 @@ int load(FILE* f, char ***out){
 	n = 1;
 
 	do{
-		inp = fgetc(f);
 		if(inp == EOF)break;
 		if(inp == SEPARATOR || inp == '\n'){
 			n++;
 			inp = fgetc(f);
-			while(inp != ' ' && inp != EOF)inp = fgetc(f);
+			while(inp == ' ' || inp == '\n')inp = fgetc(f);
 		}
-		else m[n-1]++;
+		else{
+			m[n-1]++;
+			inp = fgetc(f);
+		}
 	}while(inp != EOF);
 	
 	rewind(f);
+	inp = fgetc(f);
 	for(int i = 0; i < n; i++)names[i] = (char*)malloc((m[i] + 1) * sizeof(char));
 	for(int i = 0; i < n; i++)names[i][m[i]] = '\0';
 	n = 0;
 	int l = 0;
-	
+		double test = sqrt(9);
 	do{
-		inp = fgetc(f);
 		if(inp == EOF || inp == '\n')break;
 		if(inp == SEPARATOR){
 			n++;
 			l = 0;
-			inp = fgetc(f);
-			while(inp != ' ' && inp != EOF && inp != '\n' )inp = fgetc(f);
+		inp = fgetc(f);
+			while(inp == ' ' || inp == '\n' )inp = fgetc(f);
 		}
-		else names[n][l++] = inp;
+		else{
+			names[n][l++] = inp;
+			inp = fgetc(f);
+		}
 	}while(inp != EOF);
 	return n+1;
 	//TODO: use a for loop and a switch case instead of running while loop three times
 }
 
-struct letterdata *analyze(char **sample, size_t size){
+void analyze(char **sample, size_t size, struct stats *outdata){
 	//analyze letters in the names by which letter preceeds and succeedes it
 	size_t charwidth = 1;
 	for(size_t i = 0; i < 8 * sizeof(char); i++)charwidth *= 2;
-	struct letterdata *out = (struct letterdata*)malloc(charwidth * sizeof(struct letterdata));
+	//TODO: check if outdata->ldata is already allocated, if not only then do this
+	outdata->ltotal = 0;
+	outdata->cnames = size;
+	outdata->ldata = (struct letterdata*)malloc(charwidth * sizeof(struct letterdata));
 	for(size_t i = 0; i < charwidth; i++){
-		out[i].weight = 0;
-		out[i].succeede = (int*)malloc(charwidth * sizeof(int));
-		out[i].preceede = (int*)malloc(charwidth * sizeof(int));
-		for(size_t j = 0; j < charwidth; j++)out[i].succeede[j] = 0;
-		for(size_t j = 0; j < charwidth; j++)out[i].preceede[j] = 0;
+		outdata->ldata[i].weight = 0;
+		outdata->ldata[i].succeede = (int*)malloc(charwidth * sizeof(int));
+		outdata->ldata[i].preceede = (int*)malloc(charwidth * sizeof(int));
+		for(size_t j = 0; j < charwidth; j++)outdata->ldata[i].succeede[j] = 0;
+		for(size_t j = 0; j < charwidth; j++)outdata->ldata[i].preceede[j] = 0;
 	}
+
 	for(size_t i = 0; i < size; i++){
 		int c = -1;
 		do{
 			c++;
 			char letter = sample[i][c];
-			out[sample[i][c]].weight++;
-			if(sample[i][c])out[sample[i][c]].succeede[sample[i][c+1]]++;
+			outdata->ldata[sample[i][c]].weight++;
+			if(sample[i][c])outdata->ldata[sample[i][c]].succeede[sample[i][c+1]]++;
 			if(!c){
-				out[sample[i][c]].preceede['\0']++;
-//				out['\0'].weight++;
-//				out['\0'].succeede[sample[i][c]]++;
+				outdata->ldata[sample[i][c]].preceede['\0']++;
 			}
-			else out[sample[i][c]].preceede[sample[i][c-1]]++;
+			else outdata->ldata[sample[i][c]].preceede[sample[i][c-1]]++;
+			outdata->ltotal++;
 		}while(sample[i][c] != '\0');
 	}
-	out['\0'].weight/=8;
-	return out;
 }
 
-char *assemble(struct letterdata *letters){
+char *assemble(struct stats letters){
 	//assemble a random word based on the letter data
 	//TODO: make the assembler better utilize the data it has in function construction
 	long sum = 0;
 	size_t charwidth = 1, s = 0;
+	const double namelen = (double)letters.ltotal / letters.cnames;
 	for(size_t i = 0; i < 8 * sizeof(char); i++)charwidth *= 2;
 
 	for(size_t i = 0; i < charwidth; i++)sum+=(FSELECTOR);
@@ -131,6 +145,7 @@ char *assemble(struct letterdata *letters){
 		c = c->next;
 		c->next = NULL;
 		long lsum = 0;
+		size_t nsize = s-1;
 		for(size_t i = 0; i < charwidth; i++)lsum+=(LSELECTOR);
 		long r = random(1, lsum);
 //		printf("%d/%d\n", r, lsum);
@@ -159,7 +174,9 @@ int main(int argc, char *argv[]){
 	if(argc == 2)count = atoi(argv[1]);
 	srand(time(NULL));
 	int n = load(fopen("names-2.csv", "r"), &names);
-	struct letterdata *data = analyze(names, n);
+	struct stats data;
+	analyze(names, n, &data);
+//	for(int i = 0; i < n; i++)printf("%s\n", names[i]);
 /*
 	for(int i = 0; i < 256; i++){
 		if(data[i].weight)printf("%c(%d): %d\n", i, i, data[i].weight);
