7cc38aaec2282a8af6626d501d43ac0ebef23bbb
starmlerp <mlerp.blop@gmail.com>
2022-08-23 11:07:08 +0200

    fixed database, added cmd interface, and fixed probability caltulation

--- rnamegen.c
+++ rnamegen.c
@@ -5,14 +5,13 @@
 
 #define SEPARATOR ','
 
+#define FSELECTOR sqrt(letters.ldata[i].weight * letters.ldata[i].preceede['\0'])
+#define LSELECTOR sqrt(letters.ldata[prev].succeede[i] * letters.ldata[i].weight * (i?1:nsize/namelen))
+
 #define random(x, y) (rand() % (y - x + 1) + x)
 
-#define FSELECTOR sqrt(letters.ldata[i].weight * letters.ldata[i].preceede['\0'])
-#define LSELECTOR sqrt(letters.ldata[t].succeede[i] * letters.ldata[i].weight * letters.ldata[t].preceede[i] * (i?1:nsize/namelen))
-
 struct letterdata{
-	int weight, *preceede;
-	int *succeede;
+	int weight, *preceede, *succeede;
 };
 
 struct stats{
@@ -23,7 +22,7 @@ struct stats{
 
 struct word{
 	char letter;
-	struct word *next;
+	struct word *next, *prev;
 };
 
 int load(FILE* f, char ***out){
@@ -122,7 +121,6 @@ void analyze(char **sample, size_t size, struct stats *outdata){
 
 char *assemble(struct stats letters){
 	//assemble a random word based on the letter data
-	//TODO: make the assembler better utilize the data it has in function construction
 	long sum = 0;
 	size_t charwidth = ((unsigned char)-1)+1, s = 0;
 	const double namelen = (double)letters.ltotal / letters.cnames;
@@ -130,6 +128,7 @@ char *assemble(struct stats letters){
 	for(size_t i = 0; i < charwidth; i++)sum+=(FSELECTOR);
 	struct word *f, *c = (struct word*)malloc(sizeof(struct word));
 	c->next = NULL;
+	c->prev = NULL;
 	f = c;
 	long r = random(1, sum);
 	for(size_t i = 0; i < charwidth && r > 0; i++){
@@ -138,21 +137,21 @@ char *assemble(struct stats letters){
 	}
 	s++;
 	while(c->letter){
-		int t = c->letter;
 		c->next = (struct word*)malloc(sizeof(struct word));
+		c->next->prev = c;
 		c = c->next;
 		c->next = NULL;
 		long lsum = 0;
-		size_t nsize = s-1;
-		for(size_t i = 0; i < charwidth; i++)lsum+=(LSELECTOR);
+		const size_t nsize = s-1;
+		const char prev = c->prev->letter;
+		for(size_t i = 0; i < charwidth; i++){
+			lsum+=(LSELECTOR);
+		}
 		long r = random(1, lsum);
-//		printf("%d/%d\n", r, lsum);
 		for(size_t i = 0; i < charwidth && r > 0; i++){
 			c->letter = i;
 			r -= (LSELECTOR);
-//			printf("%d\n", r);
 		}
-//		printf("%c", c->letter);
 		s++;
 	}
 	char *out = (char*)malloc(s * sizeof(char));
@@ -168,17 +167,39 @@ char *assemble(struct stats letters){
 //TODO: implement functions to load and save letter data in a file, as well as reimplement the analyzer function in order to make it possible for it to take the existing letter data, and append to it
 int main(int argc, char *argv[]){
 	char **names = NULL;
+
+	char namedefault[50] = "data/names.csv", *namedata = namedefault;
 	int count = 8;
-	if(argc == 2)count = atoi(argv[1]);
+	
+	if(argc > 1)
+		for(int i = 1; i < argc; i++){
+			size_t l;
+			for(l = 0; argv[i][l]; ++l);
+			if(l == 2 && argv[i][0] == '-'){
+				switch(argv[i][1]){
+					case 'f':
+						namedata = argv[++i];
+					break;
+					case 'n':
+						count = atoi(argv[++i]);
+					break;
+				}
+			}
+			else{
+				printf("%s: invalid option \"%s\"\n", argv[0], argv[i]);
+						return 1;
+			}
+		}
 	srand(time(NULL));
-	int n = load(fopen("data/names.csv", "r"), &names);
+	int n = load(fopen(namedata, "r"), &names);
 	struct stats data;
 	analyze(names, n, &data);
 //	for(int i = 0; i < n; i++)printf("%s\n", names[i]);
 /*
 	for(int i = 0; i < 256; i++){
-		if(data[i].weight)printf("%c(%d): %d\n", i, i, data[i].weight);
-		for(int j = 0; j < 256; j++)if(data[i].succeede[j] || data[i].preceede[j])printf("\t%c(%d): %d:%d\n", j, j, data[i].preceede[j], data[i].succeede[j]); 
+		if(data.ldata[i].weight)printf("%c(%d): %d\n", i, i, data.ldata[i].weight);
+		for(int j = 0; j < 256; j++)if(data.ldata[i].succeede[j] || data.ldata[i].preceede[j])
+			printf("\t%c(%d): %d:%d\n", j, j, data.ldata[i].preceede[j], data.ldata[i].succeede[j]); 
 	}
 */
 //	printf("%d\n", n);
