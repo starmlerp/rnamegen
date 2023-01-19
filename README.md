0bb4295260baf7507c3af60dda6b21b3471a9e3f
starmlerp <mlerp.blop@gmail.com>
2023-01-19 22:43:23 +0100

    significantly simplified load function, renamed it to loadcsv, and updated readme

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
 # random name generator
 
-this is a small project, aiming at implementing machine learning-like algorythm in order to randomly generate names
+this is a small project, aiming at implementing machine learning-like algorythm
+in order to randomly generate names
 
+## installation
+
+NOTE: this project was made and build on linux! although it can be built and
+work on other platforms, no guide for so is present!
+
+1. download/clone the project onto your computer
+
+```bash
+git clone https://github.com/starmlerp/rnamegen
+```
+
+2. move to the project directory and build the project using `make`
+
+```bash
+cd rnamegen/
+make
+```
+
+3. run the project using the produced `main` file
+
+```bash
+chmod +x main
+./main
+```
 
--- main.cpp
+++ main.cpp
@@ -7,66 +7,43 @@
 
 #define SEPARATOR ','
 
-int load(FILE* f, char ***out){
+int loadcsv(FILE* f, char ***values){
 	rewind(f);
 	int inp, n = 1;
 	enum{
-		FC, //file size countup
-		WC, //word size countup
-		RD  //read the words
-	};
-	do{
-		inp = fgetc(f);
-		if(inp == EOF)break;
-		if(inp == SEPARATOR){
-			n++;
-			inp = fgetc(f);
-			while(inp == ' ' || inp == '\n')inp = fgetc(f);
+		SEEK, // searching through the word for the comma
+		SPAC  // searching through the spaces for the start of the word
+	}state = SPAC;
+	//obtaining an calculating the size of the file
+	fseek(f, 0, SEEK_END);
+	size_t fsize = ftell(f);
+	fseek(f, 0, SEEK_SET);
+	fsize -= ftell(f);
+	char *buffer = new char[fsize];
+	for(size_t i = 0; i < fsize; i++){
+		buffer[i]=fgetc(f);
+		if(buffer[i] == ',')n++; // increment N if we come by a comma
+	}
+	*values = new char*[n];
+	size_t ival = 0;
+	for(size_t i = 0; i < fsize; i++){
+		switch(state){
+			case SEEK:
+				if(buffer[i] == ',' || buffer[i] == '\n'){
+					state = SPAC;
+					buffer[i] = '\0';
+				}
+			break;
+			case SPAC:
+				if(buffer[i] != ' ' && buffer[i] != '\n'){
+					//NOTE: this may break if multiple immediatelly successive commas are present
+					state = SEEK;
+					(*values)[ival++] = &buffer[i];
+				}
+			break;
 		}
-	}while(inp != EOF);
-	
-	rewind(f);
-	inp = fgetc(f);
-	char **names = (char**)malloc(n * sizeof(char*));
-	*out = names;
-	int m[n];
-	for(int i = 0; i < n; i++)m[i] = 0;
-	n = 1;
-
-	do{
-		if(inp == EOF)break;
-		if(inp == SEPARATOR || inp == '\n'){
-			n++;
-			inp = fgetc(f);
-			while(inp == ' ' || inp == '\n')inp = fgetc(f);
-		}
-		else{
-			m[n-1]++;
-			inp = fgetc(f);
-		}
-	}while(inp != EOF);
-	
-	rewind(f);
-	inp = fgetc(f);
-	for(int i = 0; i < n; i++)names[i] = (char*)malloc((m[i] + 1) * sizeof(char));
-	for(int i = 0; i < n; i++)names[i][m[i]] = '\0';
-	n = 0;
-	int l = 0;
-	do{
-		if(inp == EOF || inp == '\n')break;
-		if(inp == SEPARATOR){
-			n++;
-			l = 0;
-		inp = fgetc(f);
-			while(inp == ' ' || inp == '\n' )inp = fgetc(f);
-		}
-		else{
-			names[n][l++] = inp;
-			inp = fgetc(f);
-		}
-	}while(inp != EOF);
-	return n+1;
-	//TODO: use a for loop and a switch case instead of running while loop three times
+	}
+	return n;
 }
 
 //TODO: implement functions to load and save letter data in a file, as well as reimplement the analyzer function in order to make it possible for it to take the existing letter data, and append to it
@@ -93,7 +70,7 @@ int main(int argc, char *argv[]){
 					case 'n': // number of names to produce 
 						count = atoi(argv[++i]);
 					break;
-					case 'g':
+					case 'g': // number of evaluative generations to produce
 						generations = atoi(argv[++i]);
 					break;
 					default:
@@ -108,11 +85,12 @@ prterr:
 			}
 		}
 	srand(time(NULL));
-	int n = load(fopen(namedata, "r"), &names);
+	int n = loadcsv(fopen(namedata, "r"), &names);
 	stats data;
 	data.analyze(names, n);
-	for(size_t i = 0; *names[i]; ++i){
+	for(size_t i = 0; i < n; ++i){
 		double out = data.evaluate(names[i]);
 		printf("%s: %f\n", names[i], out);
 	}
+	return 0;
 }
